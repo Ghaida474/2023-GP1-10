c6750afd4402c6510d452519c4947059ebc845a4
Munira.alhammad <128360854+Munira111@users.noreply.github.com>
2023-10-04 23:22:53 +0300

    Second commit
    Restructure the files in template + new main page

--- app/urls.py
+++ app/urls.py
@@ -5,29 +5,19 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('services', views.services, name='services'),
-    path('contact', views.contact, name='contact'),
-
     path('Home', views.Home, name='Home'),
-
-    path('client-list', views.client_list, name='client-list'),
-    path('client-new', views.client_new, name='client-new'),
-    path('client-profile', views.client_profile, name='client-profile'),
-    path('client-view', views.client_view, name='client-view'),
-    path('index5', views.index5, name='index5'),
-
-    path('index4', views.index4, name='index4'),
+    path('faculty-list', views.facultylist, name='faculty-list'),
+    path('faculty-view', views.facultyview, name='faculty-view'),
     path('project-list', views.project_list, name='project-list'),
     path('project-new', views.project_new, name='project-new'),
     path('project-overclndr', views.project_overclndr, name='project-overclndr'),
     path('project-view', views.project_view, name='project-view'),
     path('edit-profile', views.edit_profile, name='edit-profile'),
-
     path('empty-page',views.empty_page, name='empty-page'),
-    path('forgot-password-1', views.forgot_password_1, name='forgot-password-1'),
-    path('login-1', views.login_1, name='login-1'),
-    path('profile-1', views.profile_1, name='profile-1'),
-    path('reset-password-1', views.reset_password_1, name='reset-password-1'),
+    path('forgot-password', views.forgot_password, name='forgot-password'),
+    path('login', views.login, name='login'),
+    path('profile', views.profile, name='profile'),
+    path('reset-password', views.reset_password, name='reset-password'),
     path('settings', views.settings, name='settings'),
     path('chat', views.chat, name='chat'),
 
